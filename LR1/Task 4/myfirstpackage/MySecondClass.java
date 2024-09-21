700213e619ea4cab7a4cc7f7acbe658a8d97a660
Kseteny <1kseteni1@gmail.com>
2024-09-21 15:26:37 +0400

    g

---
+++ LR1/Task 4/myfirstpackage/MySecondClass.java
@@ -0,0 +1,18 @@
+package myfirstpackage;
+public class MySecondClass{
+	private int a;
+	private int b;
+	public MySecondClass() { 
+	a = 0;
+	b = 0; 
+	}
+	public MySecondClass(int A , int B) { 
+	a = A;
+	b = B; 
+	}
+	public int getA(){ return a; }
+	public int getB(){ return b; }
+	public void setA(int A) { a = A; }
+	public void setB(int B) { b = B; }
+	public int slojenie() { return a+b; }
+}
